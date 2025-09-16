d9c66a0dd085681cd47ea1f23844029a6b3c81ef
Rui Ferrão <rui@ferrao.dev>
2025-09-16 18:50:03 +0100

    post install config

---
+++ .bashrc
@@ -0,0 +1,19 @@
+# All the default Omarchy aliases and functions
+# (don't mess with these directly, just overwrite them here!)
+source ~/.local/share/omarchy/default/bash/rc
+
+# Add your own exports, aliases, and functions here.
+#
+# Make an alias for invoking commands you use constantly
+# alias p='python'
+alias vim=nvim
+alias host=dog
+
+# Use neovim as the default editor
+export EDITOR="nvim"
+
+# Set a custom prompt with the directory revealed (alternatively use https://starship.rs)
+PS1="\W \[\e]0;\w\a\]$PS1"
+
+# Load folder specific env variables
+eval "$(direnv hook bash)"
---
+++ .iex.exs
@@ -0,0 +1,29 @@
+IEx.configure(
+  colors: [
+    enabled: true,
+    eval_result: [:cyan, :bright],
+    eval_error: [:light_magenta],
+    syntax_colors: [
+      atom: :cyan,
+      boolean: :cyan,
+      charlist: :yellow,
+      nil: :magenta,
+      number: :cyan,
+      string: :green
+    ]
+  ],
+  default_prompt:
+    [
+      # a pale gold
+      "\r\e[38;5;220m",
+      # IEx context
+      "%prefix",
+      # forest green expression count
+      "\e[38;5;112m(%counter)",
+      # gold ">"
+      "\e[38;5;220m>",
+      # and reset to default colro
+      "\e[0m"
+    ]
+    |> IO.chardata_to_string()
+)
