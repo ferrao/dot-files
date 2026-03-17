e3312f90519c2fbaead2ca3fdad651c093395b04
Rui Ferrão <rui@ferrao.dev>
2026-03-17 09:50:47 +0000

    upgrades and fixes

--- .config/bashrc/alias.sh
+++ .config/bashrc/alias.sh
@@ -1,3 +1,11 @@
 # Make an alias for invoking commands you use constantly
+alias ls='eza -lh --group-directories-first --icons=auto -schanged'
 alias vim=nvim
 alias host=dog
+
+# PDFs from markdown
+md2pdf() {
+  pandoc $1 --pdf-engine=typst -V mainfont="Libertinus Serif" -o $2
+}
+
+
